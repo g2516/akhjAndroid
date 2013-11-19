d79ad4262178e0bb9d408bca07273db7556bc940
vebzical <vebzical@hotmail.com>
2013-11-19 13:26:44 +0200

    Added new layout
    carinfolayout added

--- Akhj/gen/fi/CodeRevolution/akhj/R.java
+++ Akhj/gen/fi/CodeRevolution/akhj/R.java
@@ -24,10 +24,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000c;
+        public static final int addButton=0x7f080000;
+        public static final int backButton=0x7f080001;
+        public static final int buttonbarContainer=0x7f080006;
+        public static final int carManufacturer=0x7f080008;
+        public static final int carModel=0x7f080009;
+        public static final int carName=0x7f080003;
+        public static final int carsList=0x7f08000b;
+        public static final int costName=0x7f080005;
+        public static final int costsList=0x7f080007;
+        public static final int removeButton=0x7f080002;
+        public static final int spacerLine=0x7f080004;
+        public static final int userName=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int buttonbar=0x7f030001;
+        public static final int carinfolayout=0x7f030002;
+        public static final int carlayout=0x7f030003;
+        public static final int carsrowtemplate=0x7f030004;
+        public static final int userlayout=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
